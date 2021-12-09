4d6c9d28872f11340f0beb38fbadb1a6812829c6
joaquin <jecorrea@protonmail.com>
2021-12-09 18:42:20 -0500

    partially defined spend_points route

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,14 +8,18 @@ class TransactionsController < ApplicationController
     def create
         new_transaction = Transaction.create(transaction_params)
         
-        if new_restaurant_pizza.valid?
-            render json: new_transaction.pizza, status: 201
+        if new_transaction.valid?
+            render json: new_transaction, status: 201
         else
             render json: {"errors": new_transaction.errors.full_messages}, status: 422
         end
     end
 
-    
+    def spend
+        
+    end
+
+
 
 
     private

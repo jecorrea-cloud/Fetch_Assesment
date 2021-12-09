12996b56261b59d4f05e45a8ab3032750b45404d
joaquin <jecorrea@protonmail.com>
2021-12-09 17:35:36 -0500

    partial routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :transactions
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/users', to: 'user#index'
+
+  get '/balance', to: 'transactions#index'
+  post '/add_transaction', to: 'transactions#create'
+  put '/spend_points', to: 'transactions#spend_pts'
+
+
 end
